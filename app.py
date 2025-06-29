b4b9fea01f9862d626c063f551d70e2571e8db84
rumalmethsan19 <earmethsan@students.nsbm.ac.lk>
2025-06-29 22:53:57 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+# x = 20
+# y = 30
+#
+# if x != y :
+#     print("X and Y are NOT same values.")
+#
+# if x == y :
+#     print("X and Y are same values.")
+#
+# if x == 20 and y == 30 :
+#     print("FUCK")
+#
+# else:
+#     print(" y is greater than x")
+
+#----------------------------------------------------------
+# is_hot = True
+# is_cold = False
+#
+# if is_hot :
+#     print("HOT")
+#
+# elif is_cold :
+#     print("COLD")
+
+# -------------------------------------------------------
+
+money = 1000000
+buyer_credit_good = True
+
+
+if buyer_credit_good :
+    price = money * 0.1
+    print(f"Down payment : {price}" )
+
+else:
+    price = money * 0.2
+    print(f"Down payment : {abs(price)}")
